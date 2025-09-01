bed66b4927716773c366d02d6a2cc4e3325cfed4
hsdllcw <hsdllcw@qq.com>
2025-09-02 00:36:27 +0800

    [feat] jar包可以在启动前选择启动的数据库版本

--- kblog/src/main/kotlin/io/kblog/support/config/orm/JpaConfig.kt
+++ kblog/src/main/kotlin/io/kblog/support/config/orm/JpaConfig.kt
@@ -1,10 +1,10 @@
 package io.kblog.support.config.orm
 
 import io.kblog.support.config.ContextConfig.Companion.isRunByJar
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
-import org.springframework.beans.factory.support.BeanDefinitionRegistry
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
@@ -14,50 +14,48 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import javax.persistence.EntityManagerFactory
 import javax.sql.DataSource
 
+
 /**
  * The JpaConfig class.
  * @author hsdllcw on 2020/4/29.
  * @version 1.0.0
  */
 @EnableConfigurationProperties(JpaProperties::class)
-class JpaConfig : BeanDefinitionRegistryPostProcessor {
-    lateinit var registry: BeanDefinitionRegistry
+class JpaConfig {
+    @Value("\${data-base.type}")
+    lateinit var dataBaseType: String
+
+    @Autowired
     lateinit var beanFactory: ConfigurableListableBeanFactory
 
     @Bean("dataSource")
     fun dataSource(): DataSource {
-        return if (isRunByJar) {
-            registry.removeBeanDefinition("mysqlDataSource")
+        return if (dataBaseType == "h2" || (isRunByJar && dataBaseType != "mysql")) {
             beanFactory.getBean("h2DataSource", DataSource::class.java)
         } else {
-            registry.removeBeanDefinition("h2DataSource")
             beanFactory.getBean("mysqlDataSource", DataSource::class.java)
         }
     }
 
     @Bean
-    fun entityManagerFactoryBean(builder: EntityManagerFactoryBuilder, @Qualifier("dataSource") dataSource: DataSource, jpaProperties: JpaProperties): LocalContainerEntityManagerFactoryBean {
+    fun entityManagerFactoryBean(
+        builder: EntityManagerFactoryBuilder,
+        @Qualifier("dataSource") dataSource: DataSource,
+        jpaProperties: JpaProperties
+    ): LocalContainerEntityManagerFactoryBean {
         return builder
-                .dataSource(dataSource)
-                .properties(jpaProperties.properties.apply {
-                    this["hibernate.dialect"] = if (isRunByJar) "org.hibernate.dialect.H2Dialect" else "org.hibernate.dialect.MySQL57Dialect"
-                })
-                .packages("io.kblog.domain")
-                .build()
+            .dataSource(dataSource)
+            .properties(jpaProperties.properties.apply {
+                this["hibernate.dialect"] =
+                    if (isRunByJar) "org.hibernate.dialect.H2Dialect" else "org.hibernate.dialect.MySQL57Dialect"
+            })
+            .packages("io.kblog.domain")
+            .build()
     }
 
     @Bean
     @Primary
-    fun entityManagerFactory(entityManagerFactoryBean:LocalContainerEntityManagerFactoryBean): EntityManagerFactory {
+    fun entityManagerFactory(entityManagerFactoryBean: LocalContainerEntityManagerFactoryBean): EntityManagerFactory {
         return entityManagerFactoryBean.getObject()!!
-    }
-
-
-    override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
-        this.beanFactory = beanFactory
-    }
-
-    override fun postProcessBeanDefinitionRegistry(registry: BeanDefinitionRegistry) {
-        this.registry = registry
     }
 }
